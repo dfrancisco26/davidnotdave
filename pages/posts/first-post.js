01198736f6c9f7c723d39c52c57241c4c8b7258d
David Francisco <dfrancisco2677@gmail.com>
2022-09-18 18:02:24 -0700

    Added business card image using next component.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-
+import Image from 'next/image';
 
 export default function FirstPost() {
   return (
@@ -7,11 +7,15 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-      <div class='name-banner'>
+      <div className='name-banner'>
       <h1 id='name-banner-text'>David Francisco</h1>
       </div>
-      <div class='link-boxes-div'>
-        <a target="_blank" href="https://www.linkedin.com/in/davidn0tdave/">LinkedIn</a>
+      <div className='link-boxes-div'>
+        <a id='linkedin' target="_blank" href="https://www.linkedin.com/in/davidn0tdave">LinkedIn</a>
+        <a id='github-link' target="_blank" href="https://github.com/dfrancisco26">Github</a>
+      </div>
+      <div className='business-card-div'>
+        <Image src='/Business_Card.png' width={450} height={250}></Image>
       </div>
     </>
   );
